d119805f4ffcd0d31d2b6b709604428cc7248544
ASHUPC\DELL <ashutosh.kumar@medimaze.in>
2024-06-21 20:56:48 +0530

    changes done

--- src/scenes/line/index.jsx
+++ src/scenes/line/index.jsx
@@ -5,7 +5,7 @@ import LineChart from "../../components/LineChart";
 const Line = () => {
   return (
     <Box m="20px">
-      <Header title="Line Chart" subtitle="Simple Line Chart" />
+      <Header title="Line Chart" subtitle="Yearly Analysis of Consultant Visits, Recovery Rates, and Mortality" />
       <Box height="75vh">
         <LineChart />
       </Box>
